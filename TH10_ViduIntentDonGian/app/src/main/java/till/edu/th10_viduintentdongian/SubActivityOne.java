dce160d6f2a126a1c7f49d6e710d16aa5ba01e8b
VuongHuyHoang <hoangvuong.030705@gmail.com>
2026-04-05 17:08:40 +0700

    TH10 _ViduIntentDonGian
    Hoàn thiện ví dụ intent đơn giản

---
+++ TH10_ViduIntentDonGian/app/src/main/java/till/edu/th10_viduintentdongian/SubActivityOne.java
@@ -0,0 +1,32 @@
+package till.edu.th10_viduintentdongian;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SubActivityOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sub_one);
+
+    }
+    //Hàm đáp ứng sự kiện nhấn lên nút quay về trang chủ
+    //Xử lý chuyển về màn hình trang chủ
+    public void QuayVe(View v)
+    {
+        //Tạo một đối tươn Intent
+        //Tham số thứ 2 của hàm tạo này, là tên Activity (màn hình) ta muốn chuyển sang
+        Intent iManHinhChinh = new Intent(this, MainActivity.class);
+        //Thực hiện chuyển
+        startActivity(iManHinhChinh);
+    }
+}
