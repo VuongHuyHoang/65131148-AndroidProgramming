19299528bf1db9b896297592ebf3caad379c0337
VuongHuyHoang <hoangvuong.030705@gmail.com>
2026-04-20 07:43:07 +0700

    commit lần 1
    commit lần 1

--- BaiThiGiuaKi/app/src/main/java/thigk2/VuongHuyHoang/BaiThiGiuaKi/MainActivity.java
+++ BaiThiGiuaKi/app/src/main/java/thigk2/VuongHuyHoang/BaiThiGiuaKi/MainActivity.java
@@ -1,6 +1,9 @@
 package thigk2.VuongHuyHoang.BaiThiGiuaKi;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText edtChieuDai,edtChieuRong,edtKetQua;
+    Button btnTinh;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +23,24 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        //Ánh xạ
+        edtChieuDai = findViewById(R.id.edtChieuDai);
+        edtChieuRong = findViewById(R.id.edtChieuRong);
+        edtKetQua = findViewById(R.id.edtKetQua);
+        btnTinh = findViewById(R.id.btnTinh);
+        btnTinh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                double chieudai = Double.parseDouble(edtChieuDai.getText().toString());
+                double chieuRong = Double.parseDouble(edtChieuRong.getText().toString());
+
+                double ketQua = (chieudai+chieuRong)*2;
+
+                edtKetQua.setText(ketQua+"");
+            }
         });
     }
 }
